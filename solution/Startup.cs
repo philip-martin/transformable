c1a8cc8ef67039ef4971b49505335546cf144dc8
philip-martin <pgmartin@pg-i.com>
2018-04-09 14:31:58 +0100

    Minor amends to demo CSS

--- solution/Startup.cs
+++ solution/Startup.cs
@@ -22,6 +22,7 @@ namespace transformable
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseStaticFiles(new StaticFileOptions
